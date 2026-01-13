056ae438cf4a2242d99a2b538a0427cd8257fbe9
Huesnue Turkac <turkac@gmx.net>
2026-01-13 20:20:03 +0100

    Implement simple JSON DataSource.load()

--- src/data_source.py
+++ src/data_source.py
@@ -1,6 +1,12 @@
+import json
+import csv
+import os
+
 class DataSource:
     def __init__(self, path: str):
         self.path = path
 
     def load(self):
-        raise NotImplementedError("load() must be implemented by subclasses or extended.")
+        with open(self.path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return data
