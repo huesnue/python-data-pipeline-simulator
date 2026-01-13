e0ec82f2228a17b274263178ef042155ac6a8904
Huesnue Turkac <turkac@gmx.net>
2026-01-13 21:27:13 +0100

    Add PipelineManager.run()

--- src/pipeline_manager.py
+++ src/pipeline_manager.py
@@ -1,8 +1,18 @@
 class PipelineManager:
-    def __init__(self, source, validator, transformer):
-        self.source = source
-        self.validator = validator
-        self.transformer = transformer
+    def __init__(self, data_source, data_validator, data_transformer):
+        self.data_source = data_source
+        self.data_validator = data_validator
+        self.data_transformer = data_transformer
 
     def run(self):
-        raise NotImplementedError("run() must be implemented.")
+        # 1. Load data
+        data = self.data_source.load()
+
+        # 2. Validate data
+        validated_data = self.data_validator.validate(data)
+
+        # 3. Transform data
+        transformed_data = self.data_transformer.transform(validated_data)
+
+        # 4. Return final result
+        return transformed_data
