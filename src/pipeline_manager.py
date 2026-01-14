29aebcd0261928f07ee7c498eb390360917d4d3f
Huesnue Turkac <turkac@gmx.net>
2026-01-14 14:54:12 +0100

    Refactor PipelineManager to construct components internally (CLI-ready architecture).

--- src/pipeline_manager.py
+++ src/pipeline_manager.py
@@ -1,10 +1,15 @@
 import logging
+from src.logger_config import setup_logger
+from src.data_source import DataSource
+from src.data_validator import DataValidator
+from src.data_transformer import DataTransformer
+from src.output_writer import OutputWriter
 class PipelineManager:
-    def __init__(self, data_source, data_validator, data_transformer, output_writer):
-        self.data_source = data_source
-        self.data_validator = data_validator
-        self.data_transformer = data_transformer
-        self.output_writer = output_writer
+    def __init__(self, input_path, schema_path, output_path, log_level="INFO"):
+        self.data_source = DataSource(input_path)
+        self.data_validator = DataValidator(schema_path)
+        self.data_transformer = DataTransformer()
+        self.output_writer = OutputWriter(output_path)
         self.logger = logging.getLogger(self.__class__.__name__)
 
     def run(self):
